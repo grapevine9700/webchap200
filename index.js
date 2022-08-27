fc69380e774ba4619213255dc692ccf1e732e154
grapevine9700 <99169493+grapevine9700@users.noreply.github.com>
2022-08-27 15:20:18 +0900

    비긴 프로젝트

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("되어서");
+const mechanic = [
+  { id: 1, name: "바보", model: "CORN-1234" },
+  { id: 2, name: "Harris", model: "Johnson-0077" },
+  { id: 3, name: "농가물", model: "소복녀-1155" }
+];
+
+app.get("/mechanic", (req, res) => {
+  res.json(mechanic);
 });
 
 app.listen(port, () => {
-  console.log(`서버실행 ${port}`);
+  console.log(`서버실행(${port})`);
 });
